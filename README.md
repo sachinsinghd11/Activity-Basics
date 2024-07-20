345582ec2372e804ab7e6b0e760bab94d1daa942
sachinsinghd11 <sachinsinghd11@gmail.com>
2024-07-20 19:26:51 +0530

    Add Activity methods

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This project represents how different methods of Activity works. 
+
+Methods of Activity - 
+
+- onCreate()
+- onStart()
+- onResume()
+- onRestart()
+- onPause()
+- onStop()
+- onDestroy()
--- app/src/main/java/com/example/activity_basics/MainActivity.kt
+++ app/src/main/java/com/example/activity_basics/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.activity_basics
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG,"onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG,"onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy")
     }
 }
